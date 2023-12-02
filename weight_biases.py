b01c28395d1b7f7072ecfabd291407c5ed63c9b5
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2023-12-02 17:21:24 -0300

    Adiciona fetch data

---
+++ weight_biases.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+# Download datasets
+subprocess.run(['wget', 'https://dsserver-prod-resources-1.s3.amazonaws.com/nlp/train.csv'])
+
+# Login to Weights & Biases
+subprocess.run(['wandb', 'login', '--relogin'])
+
+# Send the raw_data to the wandb and store it as an artifact
+subprocess.run([
+    'wandb', 'artifact', 'put',
+    '--name', 'tweet_classifying/train.csv',
+    '--type', 'RawData',
+    '--description', 'Real and Fake Disaster-Related Tweets Dataset',
+    'train.csv'
+])
+
+# Clean up downloaded file (optional)
+os.remove('train.csv')
