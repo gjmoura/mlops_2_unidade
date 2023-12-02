add6e036e5a73448c5c51941459567a09906a091
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2023-12-02 17:24:05 -0300

    Atualiza nomenclatura

--- fetch_data.py
+++ fetch_data.py
@@ -7,6 +7,7 @@ subprocess.run(['wget', 'https://dsserver-prod-resources-1.s3.amazonaws.com/nlp/
 # Login to Weights & Biases
 subprocess.run(['wandb', 'login', '--relogin'])
 
+# Fetch Data
 # Send the raw_data to the wandb and store it as an artifact
 subprocess.run([
     'wandb', 'artifact', 'put',
