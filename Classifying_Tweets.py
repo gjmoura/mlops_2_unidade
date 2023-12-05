03f09a51160d90bec40532b096ee215f952e8faa
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2023-12-04 23:00:27 -0300

    Update Classifying_Tweets.py

--- Classifying_Tweets.py
+++ Classifying_Tweets.py
@@ -16,6 +16,10 @@ from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow.keras.layers import TextVectorization
 
 # ------ FETCH DATA --------------------------------------------------------------------------------------
 # Download datasets
@@ -28,7 +32,7 @@ subprocess.run(['wandb', 'login', '--relogin'])
 # Send the raw_data to the wandb and store it as an artifact
 subprocess.run([
     'wandb', 'artifact', 'put',
-    '--name', 'classifying_tweets/train',
+    '--name', 'tweets_classifying/train',
     '--type', 'RawData',
     '--description', 'Real and Fake Disaster-Related Tweets Dataset',
     'train.csv'
@@ -40,10 +44,10 @@ os.remove('train.csv')
 # ------ EDA --------------------------------------------------------------------------------------
 
 # Initialize wandb run
-wandb.init(project='classifying_tweets', save_code=True)
+wandb.init(project='tweets_classifying', save_code=True)
 
 # Get the artifact
-artifact = wandb.use_artifact('mlops-ufrn/classifying_tweets/train:v0', type='RawData')
+artifact = wandb.use_artifact('mlops-ufrn/tweets_classifying/train:v0', type='RawData')
 
 # Download the content of the artifact to the local directory
 artifact_dir = artifact.download()
@@ -102,11 +106,11 @@ df['text_lemmatized'] = df['text_stop'].apply(lemmatization)
 df['final'] = df['text_lemmatized'].str.join(' ')
 
 # Salve o DataFrame como um arquivo CSV
-df.to_csv('cleanData.csv')
+df.to_csv('cleanData.csv', index=False)
 
 subprocess.run([
     'wandb', 'artifact', 'put',
-    '--name', 'classifying_tweets/clean_data',
+    '--name', 'tweets_classifying/clean_data',
     '--type', 'CleanData',
     '--description', 'Preprocessed data',
     'cleanData.csv'
@@ -114,6 +118,53 @@ subprocess.run([
 
 # Clean up downloaded file (optional)
 os.remove('cleanData.csv')
+
+# ------ DATA SEGREGATION --------------------------------------------------------------------------------------
+
+# Initialize wandb run
+run = wandb.init(project='tweet_classifying', job_type='data_segregation')
+
+# Get the clean_data artifact
+artifact = run.use_artifact('cleanData.csv:latest')
+df = pd.read_csv(artifact)
+
+X = df['final']
+y = df['target']
+
+# Split the data into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
+
+# Convert split data to DataFrames
+train_data = pd.DataFrame({'text': x_train, 'label': y_train})
+test_data = pd.DataFrame({'text': x_test, 'label': y_test})
+
+# Log the shapes of the training and testing datasets
+wandb.log({'train_data_shape': train_data.shape,
+           'test_data_shape': test_data.shape})
+
+# Save split data to CSV files
+train_data.to_csv('train_data.csv', index=False)
+test_data.to_csv('test_data.csv', index=False)
+
+# Create new artifacts for train and test data
+train_artifact = wandb.Artifact(
+    name='train_data',
+    type='TrainData',
+    description='Training data split from cleanData'
+)
+test_artifact = wandb.Artifact(
+    name='test_data',
+    type='TestData',
+    description='Testing data split from cleanData'
+)
+
+# Add CSV files to the artifacts
+train_artifact.add_file('trainData.csv')
+test_artifact.add_file('testData.csv')
+
+# Log the new artifacts to wandb
+run.log_artifact(train_artifact)
+run.log_artifact(test_artifact)
 
 wandb.finish()
 
